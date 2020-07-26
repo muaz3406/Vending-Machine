da17e8647834935cb5b13ccb3874802db79264a3
muaz3406 <muaztastemel@gmail.com>
2020-07-26 15:40:46 +0300

    PaymentController Integration Test

--- src/main/java/com/muaz/vendingmachine/utils/PaymentUtil.java
+++ src/main/java/com/muaz/vendingmachine/utils/PaymentUtil.java
@@ -1,9 +1,18 @@
 package com.muaz.vendingmachine.utils;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.muaz.vendingmachine.enums.PaymentType;
 
 public class PaymentUtil {
     public static boolean isCash(PaymentType paymentType) {
         return paymentType == PaymentType.BANKNOTE || paymentType == PaymentType.COIN;
+    }
+
+    public static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
---
+++ src/test/java/com/muaz/vendingmachine/controller/PaymentControllerTest.java
@@ -0,0 +1,42 @@
+package com.muaz.vendingmachine.controller;
+
+import com.muaz.vendingmachine.entity.PaymentRequest;
+import com.muaz.vendingmachine.service.PaymentService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static com.muaz.vendingmachine.provider.PaymentRequestProvider.getMockCardPaymentRequest;
+import static com.muaz.vendingmachine.utils.PaymentUtil.asJsonString;
+import static org.mockito.Mockito.verify;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(PaymentController.class)
+public class PaymentControllerTest {
+
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private PaymentService paymentService;
+
+    @Test
+    public void doPay() throws Exception {
+        PaymentRequest paymentRequest = getMockCardPaymentRequest();
+        mockMvc.perform(MockMvcRequestBuilders
+                .post("/payment/doPay")
+                .content(asJsonString(paymentRequest))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON));
+
+        verify(paymentService).doPay(paymentRequest);
+    }
+
+}
